d0438044b6e8bfa130a1c0b8a3ba4bddfda9e5d2
Adrian Fâciu <adrian.faciu@gmail.com>
2018-06-29 17:18:52 +0100

    Added flex layout module. Minor layout changes.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppComponent } from './app.component';
 import { TwitterService } from './services/twitter.service';
 import { InputComponent } from './input/input.component';
-import {WIPMMaterialModule} from './wipm-material.module';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {GoogleNLPService} from './services/google-nlp.service';
-import {TweetOutputListComponent} from './tweet-output-list/tweet-output-list.component';
-import {TweetCardComponent} from './tweet-card/tweet-card.component';
+import { WIPMMaterialModule } from './wipm-material.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { GoogleNLPService } from './services/google-nlp.service';
+import { TweetOutputListComponent } from './tweet-output-list/tweet-output-list.component';
+import { TweetCardComponent } from './tweet-card/tweet-card.component';
 
 @NgModule({
-  declarations: [AppComponent, InputComponent, TweetOutputListComponent, TweetCardComponent],
-  imports: [BrowserModule, HttpClientModule, WIPMMaterialModule, BrowserAnimationsModule],
+  declarations: [
+    AppComponent,
+    InputComponent,
+    TweetOutputListComponent,
+    TweetCardComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    WIPMMaterialModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule
+  ],
   providers: [TwitterService, GoogleNLPService],
   bootstrap: [AppComponent]
 })
