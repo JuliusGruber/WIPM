a92f2ac7c932e5375fe7409cecc1a461db51311a
FLusspferd <julius.gruber@chello.at>
2018-06-30 12:09:39 +0200

    check if (searchTerm)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,35 +21,40 @@ export class AppComponent implements OnInit {
   constructor(
     private twitterService: TwitterService,
     private languageService: GoogleNLPService
-  ) {}
+  ) {
+  }
 
   ngOnInit(): void {
     this.overallScore$ = of(1);
   }
 
   getTweets(searchTerm: string) {
-    this.tweets$ = this.twitterService.getTweets(searchTerm).pipe(
-      flatMap(arrayValue => from(arrayValue)),
-      flatMap(singleTweet => this.languageService.analyzeTweet(singleTweet)),
-      scan((tweetsList, tweet, index) => [...tweetsList, tweet], [])
-    );
+    console.log('Search term: ' + searchTerm);
+    if (searchTerm) {
+      this.tweets$ = this.twitterService.getTweets(searchTerm).pipe(
+        flatMap(arrayValue => from(arrayValue)),
+        flatMap(singleTweet => this.languageService.analyzeTweet(singleTweet)),
+        scan((tweetsList, tweet, index) => [...tweetsList, tweet], [])
+      );
 
-    this.positiveTweets$ = this.tweets$.pipe(
-      map(arr => arr.filter(el => el.res.documentSentiment.score > 0))
-    );
+      this.positiveTweets$ = this.tweets$.pipe(
+        map(arr => arr.filter(el => el.res.documentSentiment.score > 0))
+      );
 
-    this.neagativeTweets$ = this.tweets$.pipe(
-      map(arr => arr.filter(el => el.res.documentSentiment.score < 0))
-    );
+      this.neagativeTweets$ = this.tweets$.pipe(
+        map(arr => arr.filter(el => el.res.documentSentiment.score < 0))
+      );
 
-    this.overallScore$ = concat(
-      of(1),
-      this.tweets$.pipe(
-        map(arr =>
-          arr.map(el => Math.abs(el.res.documentSentiment.score) * 100)
-        ),
-        map(arr => arr.reduce((acc, cur) => acc + cur, 0) / arr.length)
-      )
-    );
+      this.overallScore$ = concat(
+        of(1),
+        this.tweets$.pipe(
+          map(arr =>
+            arr.map(el => Math.abs(el.res.documentSentiment.score) * 100)
+          ),
+          map(arr => arr.reduce((acc, cur) => acc + cur, 0) / arr.length)
+        )
+      );
+    }
   }
+
 }
