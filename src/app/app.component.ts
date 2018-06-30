d781b6d75baa85703a2b4076e2dd9727ef9c37a1
Adrian Fâciu <adrian.faciu@gmail.com>
2018-06-30 11:59:10 +0100

    Added copy link

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
 import { TwitterService } from './services/twitter.service';
 import { from, Observable, of, concat, interval } from 'rxjs';
 import {
@@ -29,6 +30,8 @@ export class AppComponent implements OnInit {
 
   showSpinner$: Observable<boolean>;
 
+  searchTerm = '';
+
   constructor(
     private twitterService: TwitterService,
     private languageService: GoogleNLPService
@@ -38,6 +41,19 @@ export class AppComponent implements OnInit {
     this.overallScore$ = of(1);
 
     this.showSpinner(this.twitterService.tokenReceived$);
+
+    const termName = 'term';
+    let term = window.location.search.substr(
+      window.location.search.indexOf(`${termName}=`) + termName.length + 1
+    );
+
+    if (term.includes('=')) {
+      term = term.substr(0, term.indexOf('='));
+    }
+
+    if (term) {
+      this.twitterService.tokenReceived$.subscribe(_ => this.getTweets(term));
+    }
   }
 
   private showSpinner(stopStream) {
@@ -50,10 +66,22 @@ export class AppComponent implements OnInit {
     );
   }
 
+  copyLink() {
+    const nav = navigator as any;
+    if (nav.clipboard) {
+      nav.clipboard.writeText(
+        'https://wipm-2b676.firebaseapp.com/?term=' + this.searchTerm
+      );
+    }
+  }
+
   getTweets(searchTerm: string) {
     if (!searchTerm) {
+      this.searchTerm = '';
       return;
     }
+
+    this.searchTerm = searchTerm;
 
     this.tweets$ = this.twitterService.getTweets(searchTerm).pipe(
       flatMap(arrayValue => from(arrayValue)),
